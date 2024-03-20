2a3c64765d79fba5a4acd0a5f29ad26a9e24816a
Andres Santamarina <andresesantamarina@gmail.com>
2024-03-20 01:32:15 -0300

    arregle las rutas del administrador

--- src/components/routes/RutasAdmin.jsx
+++ src/components/routes/RutasAdmin.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Administrar from "../pages/Administrar";
+import FormularioReceta from "../pages/receta/FormularioReceta";
 
 const RutasAdmin = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <>
+      <Routes>
+        <Route exact path="/" element={<Administrar />}></Route>
+        <Route
+          exact
+          path="/crear"
+          element={<FormularioReceta editar={false} titulo="Nueva receta" />}
+        ></Route>
+        <Route
+          exact
+          path="/editar/:id"
+          element={<FormularioReceta editar={true} titulo="Editar receta" />}
+        ></Route>
+      </Routes>
+    </>
+  );
 };
 
 export default RutasAdmin;
