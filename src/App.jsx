477dfaf3cf8d4cbd1b7841cee6e0121845c2acb1
Andres Santamarina <andresesantamarina@gmail.com>
2024-03-20 01:17:53 -0300

    arregle las rutas de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,38 @@ import Inicio from "./components/pages/Inicio";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import FormularioReceta from "./components/pages/receta/FormularioReceta";
 import DetalleReceta from "./components/pages/receta/DetalleReceta";
-import Administrar from "./components/pages/Administrar";
 import Error404 from "./components/pages/Error404";
+import { useState } from "react";
+import RutasProtegidas from "./components/routes/RutasProtegidas";
+import RutasAdmin from "./components/routes/RutasAdmin";
+import Login from "./components/pages/receta/Login"
 
-//npx json-server db.json
 function App() {
+  const usuario = JSON.parse(sessionStorage.getItem("usuarioRecetas")) || "";
+  const [usuarioLogueado, setUsuarioLogueado] = useState(usuario);
   return (
     <BrowserRouter>
-      <Menu />
+      <Menu
+        usuarioLogueado={usuarioLogueado}
+        setUsuarioLogueado={setUsuarioLogueado}
+      />
       <Routes>
         <Route exact path="/" element={<Inicio />}></Route>
-        <Route exact path="/administrador" element={<Administrar />}></Route>
         <Route
           exact
-          path="/administrador/crear"
-          element={<FormularioReceta editar={false} titulo="Nueva receta"/>}
+          path="/administrador"
+          element={
+            <RutasProtegidas>
+              <RutasAdmin />
+            </RutasProtegidas>
+          }
         ></Route>
-         <Route
+        <Route exact path="/detalle/:id" element={<DetalleReceta />}></Route>
+        <Route
           exact
-          path="/administrador/editar/:id"
-          element={<FormularioReceta editar={true} titulo="Editar receta"/>}
-        ></Route>
-         <Route
-          exact
-          path="/detalle/:id"
-          element={<DetalleReceta />}
+          path="/login"
+          element={<Login setUsuarioLogueado={setUsuarioLogueado} />}
         ></Route>
         <Route path="*" element={<Error404 />}></Route>
       </Routes>
@@ -41,4 +46,3 @@ function App() {
 }
 
 export default App;
-
